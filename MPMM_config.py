d35eb0fafb7bc2c7e2e0ded8ea920f0c2fbb0ba5
framboise-pi <55726262+framboise-pi@users.noreply.github.com>
2020-05-16 20:27:55 +0200

    Create MPMM_config.py

---
+++ MPMM_config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+## FONTS ## CARACTERES
+welcomefont = ('Arial', 40)
+font_date = ('Arial', 20)
+font_hour = ('Arial', 80)
+font_minutes = ('Arial', 80)
+font_secondes = ('Arial', 20)
+
+## TEXT ## TEXTES
+welcome = 'My Pi Magic Mirror'
+
+## SENSEHAT ##
+sensehat_on = True
+sensehat_rotation = 270
+sensehat_welcome = 'Welcome back !'
+
+## CLOCK ## HORLOGE
+days = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
+months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Decembre"]
+
+## MODULE WORDS ##
+module_words_on = True
