8e297bcac6ae370db4b410177169fc1506ddecde
framboise-pi <55726262+framboise-pi@users.noreply.github.com>
2020-05-16 20:33:07 +0200

    Create __init__.py

---
+++ sensehat_pictures/__init__.py
@@ -0,0 +1,39 @@
+blanc = (255,255,255)
+bleu = (0, 0, 255)
+gris=(96,96,96)
+jaune = (255, 255, 0)
+marron=(88,41,0)
+orange=(237,127,16)
+rose = (253,108,158)
+rouge = (255, 0, 0)
+vert = (0, 255, 0)
+vide = (0,0,0)
+violet=(102,0,153)
+
+O = vide
+B = bleu
+J = jaune
+R = rouge
+S = rose
+V = vert
+
+monstre_01 = [
+R, O, O, R, R, O, O, R,
+R, O, R, R, R, R, O, R,
+O, R, J, R, R, J, R, O,
+O, R, R, R, R, R, R, O,
+O, R, O, O, O, O, R, O,
+O, O, R, R, R, R, O, O,
+R, R, O, R, R, O, R, R,
+R, O, O, O, O, O, O, R,
+]
+monstre_02 = [
+O, O, R, O, O, R, O, O,
+O, O, R, R, R, R, O, O,
+O, O, R, R, R, R, O, O,
+O, R, O, R, R, O, R, O,
+R, O, R, R, R, R, O, R,
+O, O, R, R, R, R, O, O,
+R, R, O, R, R, O, R, R,
+R, O, O, O, O, O, O, R,
+]
